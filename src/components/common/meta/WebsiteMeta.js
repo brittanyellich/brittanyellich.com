eb4ddbbd13003765700d2306d5f9e230fca82dd7
Brittany Heller <brittany.heller@live.com>
2019-08-18 19:08:00 -0700

    Changes to formatting of script tag

--- src/components/common/meta/WebsiteMeta.js
+++ src/components/common/meta/WebsiteMeta.js
@@ -65,10 +65,10 @@ const WebsiteMeta = ({ data, settings, canonical, title, description, image, typ
                 `}</script>
                 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                 <script>
-                    (adsbygoogle = window.adsbygoogle || []).push({
-                        google_ad_client: "ca-pub-6529422608874960",
-                    enable_page_level_ads: true
-                });
+                    (adsbygoogle = window.adsbygoogle || []).push({`
+                        "google_ad_client": "ca-pub-6529422608874960",
+                        "enable_page_level_ads": true
+                    `});
                 </script>
             </Helmet>
             <ImageMeta image={shareImage} />
