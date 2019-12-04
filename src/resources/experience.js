d1ff5a7a0099e7e1e60a91922961590904026c7f
Brittany Heller <brittany.heller@live.com>
2019-12-03 21:28:23 -0800

    Add RS experience

--- src/resources/experience.js
+++ src/resources/experience.js
@@ -1,56 +1,70 @@
 module.exports = [
     {
-        "Id": 1,
-        "JobTitle": "Software Engineer",
-        "Company": "Pro Unlimited (Nike Contractor)",
-        "ImgUrl": "ImgUrl",
-        "Timeline": "2019-Present",
-        "Description": "Full-stack Java and React engineer, maintaining existing applications and building new features in the customization space. Working on a diverse devops team supporting a $250M business for Nike",
-        "Skills": [
-            "Java",
-            "Spring",
-            "React",
-            "AWS"
-        ]
+        Id: 1,
+        JobTitle: `Software Engineer`,
+        Company: `Pro Unlimited (Nike Contractor)`,
+        ImgUrl: `ImgUrl`,
+        Timeline: `2019-Present`,
+        Description: `Full-stack Java and React engineer, maintaining existing applications and building new features in the customization space. Working on a diverse devops team supporting a $250M business for Nike`,
+        Skills: [
+            `Java`,
+            `Spring`,
+            `React`,
+            `AWS`,
+        ],
     },
     {
-        "Id": 2,
-        "JobTitle": "Software Development Consultant",
-        "Company": "Allata",
-        "ImgUrl": "ImgUrl",
-        "Timeline": "2018-2019",
-        "Description": "Full-stack .NET developer designing and building solutions for a large customer in the real estate industry. Heavily AWS and C#/.NET serverless solutions using SQS queues and lambdas.",
-        "Skills": [
-            "C#",
-            ".NET",
-            "SQL",
-            "AWS"
-        ]
+        Id: 2,
+        JobTitle: `Software Development Consultant`,
+        Company: `Allata`,
+        ImgUrl: `ImgUrl`,
+        Timeline: `2018-2019`,
+        Description: `Full-stack .NET developer designing and building solutions for a large customer in the real estate industry. Heavily AWS and C#/.NET serverless solutions using SQS queues and lambdas.`,
+        Skills: [
+            `C#`,
+            `.NET`,
+            `SQL`,
+            `AWS`,
+        ],
     },
     {
-        "Id": 3,
-        "JobTitle": "Risk Control Consultant II",
-        "Company": "Liberty Mutual Insurance",
-        "ImgUrl": "ImgUrl",
-        "Timeline": "2016-2018",
-        "Description": "Consulted on property and casualty risk, providing solutions for customers and advice to underwriters.",
-        "Skills": [
-            "Risk Assessment",
-            "Root Cause Analysis"
-        ]
+        Id: 3,
+        JobTitle: `Software Developer`,
+        Company: `Reaper Studios`,
+        ImgUrl: `ImgUrl`,
+        Timeline: `2017-2018`,
+        Description: `Part-time contractor for very early-stage, bootstrapped mobile application and gaming startup. Focused primarily on Android development and 2D mobile games through Unity.`,
+        Skills: [
+            `Android`,
+            `Java`,
+            `Unity`,
+            `C#`,
+        ],
     },
     {
-        "Id": 4,
-        "JobTitle": "Graduate Research Assistant",
-        "Company": "Oregon State University",
-        "ImgUrl": "ImgUrl",
-        "Timeline": "2014-2016",
-        "Description": "Conducted research activities for public health faculty focusing on occupational and environmental health, including developing eLearning content and data gathering for pollution GIS mapping.",
-        "Skills": [
-            "GIS",
-            "Database Management",
-            "Adobe Creative Suite"
-        ]
-    }
+        Id: 4,
+        JobTitle: `Risk Control Consultant II`,
+        Company: `Liberty Mutual Insurance`,
+        ImgUrl: `ImgUrl`,
+        Timeline: `2016-2018`,
+        Description: `Consulted on property and casualty risk, providing solutions for customers and advice to underwriters.`,
+        Skills: [
+            `Risk Assessment`,
+            `Root Cause Analysis`,
+        ],
+    },
+    {
+        Id: 5,
+        JobTitle: `Graduate Research Assistant`,
+        Company: `Oregon State University`,
+        ImgUrl: `ImgUrl`,
+        Timeline: `2014-2016`,
+        Description: `Conducted research activities for public health faculty focusing on occupational and environmental health, including developing eLearning content and data gathering for pollution GIS mapping.`,
+        Skills: [
+            `GIS`,
+            `Database Management`,
+            `Adobe Creative Suite`,
+        ],
+    },
     
 ]
